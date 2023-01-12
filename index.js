42f451e2ade69ff0bc227117a0dec239f9522aa4
anzudevelop <72302256+anzudevelop@users.noreply.github.com>
2023-01-12 09:42:18 +0700

    uploading

---
+++ index.js
@@ -0,0 +1,34 @@
+const xlsx = require('xlsx');
+var fs = require('fs');
+
+function convertExcelFileToJsonUsingXlsx() {
+
+  // Read the file using pathname
+  const file = xlsx.readFile('./xlsx/Data.xlsx');
+
+  // Grab the sheet info from the file
+  const sheetNames = file.SheetNames;
+  const totalSheets = sheetNames.length;
+
+  // Variable to store our data
+  let parsedData = [];
+
+  // Loop through sheets
+  for (let i = 0; i < totalSheets; i++) {
+
+      // Convert to json using xlsx
+      const tempData = xlsx.utils.sheet_to_json(file.Sheets[sheetNames[i]]);
+
+      // Skip header row which is the colum names
+      tempData.shift();
+
+      // Add the sheet's json to our data array
+      parsedData.push(...tempData);
+  }
+
+ // call a function to save the data in a json file
+
+ return(parsedData);
+}
+
+console.log(convertExcelFileToJsonUsingXlsx())
