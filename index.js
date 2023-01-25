b1593534dcf2ca0a6b5afbb1b266ef7df314b2e0
Alexey Kosogor <a.kosogor@pixlpark.com>
2023-01-25 15:05:59 +0700

    work-time

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const xlsx = require('xlsx');
 var fs = require('fs');
+const path = require('path')
 
 function convertExcelFileToJsonUsingXlsx() {
 
   // Read the file using pathname
-  const file = xlsx.readFile('./xlsx/Data.xlsx');
+  const file = xlsx.readFile('./xlsx/data.xlsx');
 
   // Grab the sheet info from the file
   const sheetNames = file.SheetNames;
@@ -28,7 +29,90 @@ function convertExcelFileToJsonUsingXlsx() {
 
  // call a function to save the data in a json file
 
- return(parsedData);
+ return(JSON.stringify(parsedData));
 }
 
-console.log(convertExcelFileToJsonUsingXlsx())
+const getTimeByPartOfTime = (part) => {
+  let h = Math.floor(part * 24)
+  let m = Math.floor(((part * 24) % 1) * 60)
+  let s = Math.floor(((((part * 24) % 1) * 60) % 1) * 60)
+  return {h, m, s}
+}
+
+const findIndexOfDate = (data, day) => {
+  for(let i = 0; i < data.length; i++) {
+    if(data[i]["Дата"].split(".")[0] == day) return i
+  }
+}
+
+const executeWorkTimeFromDate = (day) => {
+  let secondsInDay = 24 * 60 * 60
+  let h, m, s
+  h = Number(day['Пришел'].split(':')[0]) * 3600
+  m = Number(day['Пришел'].split(':')[1]) * 60
+  s = Number(day['Пришел'].split(':')[2])
+  let startWork = h + m + s
+  h = Number(day['Перерыв'].split(':')[0]) * 3600
+  m = Number(day['Перерыв'].split(':')[1]) * 60
+  s = Number(day['Перерыв'].split(':')[2])
+  let lunchTime = h + m + s
+  h = Number(day['Ушел'].split(':')[0]) * 3600
+  m = Number(day['Ушел'].split(':')[1]) * 60
+  s = Number(day['Ушел'].split(':')[2])
+  let endDay = h + m + s
+  return ((endDay - lunchTime - startWork) / secondsInDay)
+}
+
+const getFullMonthTime = () => {
+  let data = JSON.parse(convertExcelFileToJsonUsingXlsx())
+  let fullTime = 0
+  for(let i = 0; i < data.length - 1; i++) {
+    if(data[i]["Итого"] != 0 || data[i].hasOwnProperty('Пришел')) {
+      fullTime += data[i]["Итого"]
+      if(data[i]["Итого"] == 0) {
+        fullTime += executeWorkTimeFromDate(data[i])
+      }
+    }
+  }
+  return fullTime
+}
+
+const isWorkDay = (d) => {
+  let dayWeek = new Date(`${d.slice(3, 5)}.${d.slice(0, 2)}.${d.slice(6, 10)}`).toLocaleString("en-us", { weekday: "short"})
+  if(dayWeek != 'Sat' && dayWeek != 'Sun') {
+    return true
+  }
+  return false
+}
+
+const getWorkDays = () => {
+  let data = JSON.parse(convertExcelFileToJsonUsingXlsx())
+  let daysCounter = 0
+  for(let i = 0; i < data.length - 1; i++) {
+    if((data[i]["Итого"] != 0 || data[i].hasOwnProperty('Пришел')) && isWorkDay(data[i]['Дата'])) {
+      daysCounter++
+    }
+  }
+  return daysCounter
+}
+
+const print = () => {
+  //let data = JSON.parse(convertExcelFileToJsonUsingXlsx())
+  //let day = 19
+  //let dayIndex = findIndexOfDate(data, day)
+  
+  let partOfMonthTime = getFullMonthTime() - ((8 / 24) * getWorkDays())
+  let status = ""
+  if (partOfMonthTime > 0) {
+    status = "Время переработки: "
+  } else {
+    status = "Осталось доработать: "
+    partOfMonthTime *= -1
+  }
+  let timeData = getTimeByPartOfTime(partOfMonthTime)
+  let {h, m, s} = timeData
+  console.log(`${status}${(h / 10) > 1 ? "" : "0"}${h}:${(m / 10) > 1 ? "" : "0"}${m}:${(s / 10) > 1 ? "" : "0"}${s}`)
+  
+}
+
+print()
