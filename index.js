2964f81d6f524338c5fc82827954665e02ada1c6
Alexey Kosogor <a.kosogor@pixlpark.com>
2023-03-16 12:32:41 +0700

    fix

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ const getEndTime = () => {
       h += 1
     } else m+= 30
     let endTime = `${(h + 8) < 10 ? `0${(h + 8)}` : (h + 8)}:${m < 10 ? `0${m}` : m}:${s < 10 ? `0${s}` : s}`
-    let endWork = new Date(year, month - 1, day, h + 8, m, s)
+    let endWork = new Date(year, month - 1, day, h + 7, m, s)
     let difference = ((endWork - now)/1000).toFixed(0)
     let complete = false
     if(difference < 0) {
